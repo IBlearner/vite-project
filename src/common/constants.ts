90ff3c302876a7f9a45e1aa738fb76d06bbd0721
kienvi <kienvi96@hotmail.com>
2023-03-04 13:38:31 +1000

    Adding common files.

---
+++ src/common/constants.ts
@@ -0,0 +1,22 @@
+export const views = {
+    home: {
+        name: "home",
+        routeName: "home",
+        friendlyName: "Home"
+    },
+    contact: {
+        name: "contact",
+        routeName: "contact",
+        friendlyName: "Contact me"
+    },
+    about: {
+        name: "about",
+        routeName: "about",
+        friendlyName: "About me"
+    },
+    reviews: {
+        name: "reviews",
+        routeName: "reviews",
+        friendlyName: "Reviews"
+    }
+}
