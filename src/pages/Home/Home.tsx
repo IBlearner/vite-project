641f79507d24f9eb3510b91e6087819f225bb174
svanbrugh <svanbrugh@outlook.com>
2023-03-15 21:19:38 +1000

    Changed styling on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,15 +3,16 @@ import "./Home.scss";
 const Home = () => {
     return (
         <div>
-            <h1 id="Home">DUC TINH LAM</h1>
+            <div className="header">
+                <img src = "LAM SAFETY.png" alt="Driving School" />
+                <h1 id="Home">DUC TINH LAM</h1> 
+            </div>
+             
             <h1 id="viethome">trang chủ</h1>
-            <h2 id="mee">heading</h2>
-            <img id="solarpanels"src="https://static.vecteezy.com/system/resources/previews/015/317/769/original/solar-panels-semi-flat-color-object-alternative-energy-technology-editable-elements-full-sized-items-on-white-simple-cartoon-style-illustration-for-web-graphic-design-and-animation-vector.jpg"></img>
-            <img id="solar"src="https://static.vecteezy.com/system/resources/previews/015/317/769/original/solar-panels-semi-flat-color-object-alternative-energy-technology-editable-elements-full-sized-items-on-white-simple-cartoon-style-illustration-for-web-graphic-design-and-animation-vector.jpg"></img>
-            <p>Lorem i like coke Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <h2>heading 2</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <img src="https://scontent.fbne9-1.fna.fbcdn.net/v/t1.6435-9/69113919_106987183999721_3730680598432317440_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=RPCCttH0UOgAX_wAIMG&_nc_ht=scontent.fbne9-1.fna&oh=00_AfBd78xgy5M14zP6nR4VvGxxmAqBHhESYtF5P7Og3vOYZQ&oe=6426B321" alt="Cute white puppy" width="190" height="200"></img>
+            <h2 id="head2">heading</h2>
+            <p id = "abut.me">Lorem i like coke Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <h2 id = "head3">heading 2</h2>
+            <p id = "abut.me2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
 
         
